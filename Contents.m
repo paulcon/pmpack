d8c4741e549e7595e465078e9f2d8fb9416ef225
= <dgleich@stanford.edu>
2010-05-19 12:58:33 -0700

    Changed pmpack to file exchange select structure:
    http://matlabwiki.mathworks.com/File_Exchange_Submission_Guidelines
    
    and generated a contents file with:
    
    http://www.mathworks.com/access/helpdesk/help/techdoc/matlab_env/f9-6232.html#f9-6120

---
+++ Contents.m
@@ -0,0 +1,29 @@
+% Parameterized Matrix Package
+% Version 1.0 (pre) 
+%
+% Approximation methods
+% ---------------------
+%   pseudospectral          - PSEUDOSPECTRAL
+%   spectral_galerkin       - 
+%
+% Utility functions
+% -----------------
+%   error_estimate          - 
+%   evaluate_expansion      - Evaluates a polynomial expansion at a point.
+%   evaluate_ops            - Evaluate the n-vector of orthonormal polynomials at a point.
+%   galerkin_preconditioner - 
+%   gaussian_quadrature     - GAUSSRULE Compute a Gaussian quadrature rule
+%   hermite_parameter       - HERMITE_PARAM Construct a parameter with a Hermite weight function
+%   index_set               - 
+%   jacobi_eigenvecs        - Construct the eigenvector matrix of the Jacobi matrix 
+%   jacobi_matrix           - Construct a Jacobi matrix for a parameter.
+%   jacobi_parameter        - Construct a parameter with a Jacobi weight function
+%   kronmult                - Efficient Kronecker Multiplication 
+%   midpoint                - 
+%   minimum_coefficient     - 
+%   parameter               - Construct a parameter
+%   residual_error_estimate - RESID Compute the residual sqrt(\int r'*r), where r=A*X.U*basis-f.
+%   sort_bases              - Sorts the multivariate bases in order of total degree of basis
+%   total degree of basis
+
+% Copyright, 2010, Paul G. Constantine and David F. Gleich
---
+++ test/test_main.m
@@ -0,0 +1,6 @@
+function test_main
+
+galerkin_testscript;
+pseudospec_testscript;
+utils_testscript;
+
