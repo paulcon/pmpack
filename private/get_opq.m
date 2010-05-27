92d4ce429ef7a2ed795032d4bb21f139478ecc26
David Gleich <dgleich@mandelbrot.ran.sandia.gov>
2010-05-27 12:02:21 -0700

    Added Gautschi's missing OPQ code

---
+++ private/get_opq.m
@@ -0,0 +1,4 @@
+% Get codes from Gautschi's OPQ collection that make pmpack work.
+% These are included into the repository and this script is provided purely
+% for documentation purposes.
+urlwrite('http://www.cs.purdue.edu/archives/2002/wxg/codes/r_hermite.m','r_hermite.m');
