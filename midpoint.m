3d7f9cf462cea74f5d122f5d28cb5011c5f7871b
David Gleich <dgleich@fluffy2.(none)>
2010-06-11 09:38:32 -0700

    Fixed error in midpoint

--- midpoint.m
+++ midpoint.m
@@ -4,5 +4,5 @@ dim=length(s);
 m=zeros(1,dim);
 for i=1:dim
     if ~isfield(s,'r') || ~isfield(s,'l'), error('Cannot determine midpoint.'); end
-    m(i)=0.5*(s(i).r-s(i).l);
+    m(i)=0.5*(s(i).r-s(i).l)+s(i).l;
 end
